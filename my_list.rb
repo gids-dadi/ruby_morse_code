a395ed35bfa66c29126420d82ba680f853c2652d
bruk19 <brukteshome21@gmail.com>
2022-08-26 09:49:14 +0300

    Update Created instance of Mylist

--- my_list.rb
+++ my_list.rb
@@ -17,9 +17,9 @@ class MyList
   end
 end
 
-list = MyList.new(1, 2, 3, 4)
-list.all? { |e| e < 5 }
-list.all? { |e| e > 5 }
-list.any? { |e| e == 2 }
-list.any? { |e| e == 5 }
-list.filter? { |e| e.even? }
+list_test = MyList.new(1, 2, 3, 4)
+list_test.all? { |e| e < 5 }
+list_test.all? { |e| e > 5 }
+list_test.any? { |e| e == 2 }
+list_test.any? { |e| e == 5 }
+list_test.filter? { |e| e.even? }
